1579774fc6bffd786f420d11b4c91e83ac9008d0
Khalid Ansari <khaliljpc@gmail.com>
2023-06-20 19:49:49 +0530

    added media queries

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 'use strict';
 
-const sidebar = document.querySelector('.sidebar');
-// const active = document.querySelector('.active');
-// console.log(active);
+const sideMenu = document.querySelector('.aside');
+const menuBtn = document.querySelector('.menu-btn');
+const closeBtn = document.querySelector('.close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
 
-sidebar.addEventListener('click', function (e) {
-    const active = e.target.closest('.dash-option.active');
-    console.log(active);
-    // active.classList.remove('active');
-    // active.classList.remove('active');
-    const sidebarOption = e.target.closest('.dash-option');
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
 
-    sidebarOption.classList.add('active');
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 });
